0422f0fa70a7c79bb75502e389666a503496abef
yanmastra <yanmastra59@gmail.com>
2017-12-09 02:42:46 +0800

    modifikasi database

--- db_puskesmas.sql
+++ db_puskesmas.sql
@@ -1,54 +1,16 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.1.13-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win32
--- HeidiSQL Version:             9.4.0.5125
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+DROP DATABASE IF EXISTS `db_puskesmas2`;
+CREATE DATABASE IF NOT EXISTS `db_puskesmas2` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `db_puskesmas2`;
 
--- Dumping database structure for db_puskesmas
-DROP DATABASE IF EXISTS `db_puskesmas`;
-CREATE DATABASE IF NOT EXISTS `db_puskesmas` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `db_puskesmas`;
-
--- Dumping structure for view db_puskesmas.antrian_aktif
-DROP VIEW IF EXISTS `antrian_aktif`;
--- Creating temporary table to overcome VIEW dependency errors
-CREATE TABLE `antrian_aktif` (
-	`Username` VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
-	`Nomor Antrian` VARCHAR(16) NOT NULL COLLATE 'latin1_swedish_ci',
-	`Nama Lengkap` VARCHAR(100) NOT NULL COLLATE 'latin1_swedish_ci',
-	`NIK` CHAR(16) NOT NULL COLLATE 'latin1_swedish_ci',
-	`Keluhan` TINYTEXT NOT NULL COLLATE 'latin1_swedish_ci',
-	`Tanggal Masuk` DATETIME NOT NULL
-) ENGINE=MyISAM;
-
--- Dumping structure for view db_puskesmas.antrian_tidak_datang
-DROP VIEW IF EXISTS `antrian_tidak_datang`;
--- Creating temporary table to overcome VIEW dependency errors
-CREATE TABLE `antrian_tidak_datang` (
-	`id_antrian` VARCHAR(16) NOT NULL COLLATE 'latin1_swedish_ci',
-	`pasien` CHAR(16) NOT NULL COLLATE 'latin1_swedish_ci',
-	`keluhan` TINYTEXT NOT NULL COLLATE 'latin1_swedish_ci',
-	`tanggal_masuk` DATETIME NOT NULL,
-	`status` ENUM('ngantre','keluar','tidak datang') NOT NULL COLLATE 'latin1_swedish_ci',
-	`tanggal_keluar` DATETIME NULL,
-	`pegawai` CHAR(16) NULL COLLATE 'latin1_swedish_ci',
-	`no_telp` VARCHAR(14) NULL COLLATE 'latin1_swedish_ci',
-	`nama_lengkap` VARCHAR(100) NOT NULL COLLATE 'latin1_swedish_ci',
-	`alamat` VARCHAR(200) NULL COLLATE 'latin1_swedish_ci'
-) ENGINE=MyISAM;
-
--- Dumping structure for table db_puskesmas.dt_antrian
 DROP TABLE IF EXISTS `dt_antrian`;
 CREATE TABLE IF NOT EXISTS `dt_antrian` (
-  `id_antrian` varchar(16) NOT NULL,
+  `id_antrian` varchar(20) NOT NULL,
   `pasien` char(16) NOT NULL,
   `keluhan` tinytext NOT NULL,
   `tanggal_masuk` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -63,22 +25,22 @@ CREATE TABLE IF NOT EXISTS `dt_antrian` (
   CONSTRAINT `FK_dt_antrian_dt_pegawai` FOREIGN KEY (`pegawai`) REFERENCES `dt_pegawai` (`no_pegawai`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table db_puskesmas.dt_antrian: ~9 rows (approximately)
 DELETE FROM `dt_antrian`;
 /*!40000 ALTER TABLE `dt_antrian` DISABLE KEYS */;
 INSERT INTO `dt_antrian` (`id_antrian`, `pasien`, `keluhan`, `tanggal_masuk`, `status`, `tanggal_keluar`, `pegawai`, `no_telp`) VALUES
-	('201712010001', '45645764567456', 'sjkdbkbksjd sjdhbs sdhjcgys shjdgc ', '2017-12-08 22:22:33', 'tidak datang', '2017-12-02 13:02:27', '1293991', '87349827'),
-	('201712010002', '45645764567456', 'adkbjshbd sdbhsjdjsh sjdhgu sdhjfguys ', '2017-11-30 22:53:49', 'keluar', '2017-12-08 06:59:17', '1293991', '87687578578'),
-	('201712020001', '45645764567456', 'ksdjhfkbhsiufb', '2017-12-02 11:25:47', 'keluar', '2017-12-08 07:04:37', '1293991', '345345'),
-	('201712020002', '45645764567456', 'sdjbfkjsbfdsdjbk sdbfs ', '2017-12-02 11:43:33', 'keluar', '2017-12-08 07:06:36', '1293991', '345346384768'),
-	('201712020003', '9283479862398496', 'swfhisuf sdjfhuydf sjkdkhfuyf skjdbuy ', '2017-12-08 12:24:43', 'tidak datang', NULL, NULL, '982349823'),
-	('201712020004', '2389798126', 'kjehfiguif sjdhbfyuf skdhfuie ', '2017-12-02 12:34:16', 'ngantre', '2017-12-08 06:30:30', NULL, '23847289364'),
-	('201712040001', '45645764567456', 'Meriang singkaruan', '2017-12-04 23:46:25', 'ngantre', '2017-12-08 06:30:40', NULL, ''),
-	('201712050001', '45645764567456', 'Kebus dingin aduh-aduh', '2017-12-05 11:15:04', 'ngantre', '2017-12-08 06:30:44', NULL, '4'),
-	('201712050002', '9283479862398496', 'Meriang singkaruan', '2017-12-05 11:15:51', 'ngantre', '2017-12-08 06:36:00', NULL, '4');
+	('20171208000504', '9283479862398496', 'ketug-ketug bayune ulian sakit hati', '2017-12-08 19:46:54', 'keluar', '2017-12-08 19:50:27', '1293991', '14045'),
+	('20171208000505', '2389798126', 'Meriang singkaruan, keneh uyang ngenehin iluh', '2017-12-08 19:46:56', 'keluar', '2017-12-08 19:50:28', '1293991', '14045'),
+	('20171208000506', '23535634693874', 'Meriang singkaruan, keneh uyang ngenehin iluh', '2017-12-08 19:46:58', 'keluar', '2017-12-08 19:50:29', '1293991', '14025'),
+	('20171208000604', '23535634693874', 'Meriang singkaruan, keneh uyang ngenehin iluh', '2017-12-08 20:09:49', 'keluar', '2017-12-08 20:10:42', '1293991', '14045'),
+	('20171209000104', '23535634693874', 'Meriang singkaruan, keneh uyang ngenehin iluh', '2017-12-09 00:31:33', 'keluar', '2017-12-09 00:32:10', '1293991', '14045'),
+	('20171209000502', '45645764567456', 'Meriang singkaruan', '2017-12-09 00:34:41', 'keluar', '2017-12-09 00:35:08', '1293991', '14045'),
+	('20171209000503', '235356346', 'Kebus dingin aduh-aduh', '2017-12-09 00:34:47', 'keluar', '2017-12-09 00:40:16', '1293991', '14045'),
+	('20171209000601', '2389798126', 'ketug-ketug bayune ulian sakit hati', '2017-12-09 00:40:32', 'keluar', '2017-12-09 00:40:53', '1293991', '123456'),
+	('20171209000602', '23535634693874', 'Meriang singkaruan, keneh uyang ngenehin iluh', '2017-12-09 00:40:46', 'keluar', '2017-12-09 00:40:54', '1293991', '14045'),
+	('201712090007', '23535634693874', 'Hbcx', '2017-12-09 01:11:59', 'keluar', '2017-12-09 01:12:22', '1293991', ''),
+	('20171209000701', '2389798126', 'Meriang singkaruan, keneh uyang ngenehin iluh', '2017-12-09 01:10:40', 'keluar', '2017-12-09 01:10:55', '1293991', '13243546');
 /*!40000 ALTER TABLE `dt_antrian` ENABLE KEYS */;
 
--- Dumping structure for table db_puskesmas.dt_pasien
 DROP TABLE IF EXISTS `dt_pasien`;
 CREATE TABLE IF NOT EXISTS `dt_pasien` (
   `username` varchar(50) NOT NULL,
@@ -94,16 +56,16 @@ CREATE TABLE IF NOT EXISTS `dt_pasien` (
   CONSTRAINT `FK_dt_pasien_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table db_puskesmas.dt_pasien: ~3 rows (approximately)
 DELETE FROM `dt_pasien`;
 /*!40000 ALTER TABLE `dt_pasien` DISABLE KEYS */;
 INSERT INTO `dt_pasien` (`username`, `nik`, `nama_lengkap`, `tgl_lahir`, `tempat_lahir`, `alamat`, `no_telp`) VALUES
 	('adip', '9283479862398496', 'Adi Parmawan', '0000-00-00', 'skdjhcgsgd', 'sdjhcbsdbcgsdigisdic sjdjcbsdgic sdjcbsbdgiuc sdjbcsbdc sjhdbch', '2325345622'),
 	('adiparmawan2', '45645764567456', 'Adi Parmawan 2', '0000-00-00', 'sdkhghsjgdu', 'skdjhfshdfkhsdihfihdf sjdhfbksgdifu sdjfbsbdfi sdjfbsbdifgsd fsjdbfb', '2353456346'),
-	('wirnat', '2389798126', 'Wiranatha', '0000-00-00', 'Karangasem', 'Karangasem Selat', '9863876234');
+	('wirnat', '2389798126', 'Wiranatha', '0000-00-00', 'Karangasem', 'Karangasem Selat', '9863876234'),
+	('yanmastra', '23535634693874', 'Wayan Mastra', '1998-09-16', 'Ketewel', 'skjdfkjshdkfhk', '083119304230'),
+	('yogadar', '235356346', 'Yoga Dharma', '1998-12-16', 'Ungasan', 'xvbdfb sdbashjdg ajshdguyasd jahsgdyud kajshdiuad ajshgdad aisudb', '912852');
 /*!40000 ALTER TABLE `dt_pasien` ENABLE KEYS */;
 
--- Dumping structure for table db_puskesmas.dt_pegawai
 DROP TABLE IF EXISTS `dt_pegawai`;
 CREATE TABLE IF NOT EXISTS `dt_pegawai` (
   `username` varchar(60) NOT NULL,
@@ -118,28 +80,12 @@ CREATE TABLE IF NOT EXISTS `dt_pegawai` (
   CONSTRAINT `FK_dt_pegawai_user` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table db_puskesmas.dt_pegawai: ~1 rows (approximately)
 DELETE FROM `dt_pegawai`;
 /*!40000 ALTER TABLE `dt_pegawai` DISABLE KEYS */;
 INSERT INTO `dt_pegawai` (`username`, `no_pegawai`, `nama_lengkap`, `tempat_lahir`, `alamat`, `jenis_kelamin`, `no_telp`) VALUES
 	('adi', '1293991', 'Adi Parmawan', 'Ungasan', 'Ungasan', 'Laki-laki', '298787');
 /*!40000 ALTER TABLE `dt_pegawai` ENABLE KEYS */;
 
--- Dumping structure for view db_puskesmas.riwayat_antrian
-DROP VIEW IF EXISTS `riwayat_antrian`;
--- Creating temporary table to overcome VIEW dependency errors
-CREATE TABLE `riwayat_antrian` (
-	`Username Pasien` VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
-	`Nomor Antrian` VARCHAR(16) NOT NULL COLLATE 'latin1_swedish_ci',
-	`Nama Lengkap` VARCHAR(100) NOT NULL COLLATE 'latin1_swedish_ci',
-	`Keluhan` TINYTEXT NOT NULL COLLATE 'latin1_swedish_ci',
-	`Tanggal Keluar` DATETIME NULL,
-	`Alamat` VARCHAR(200) NULL COLLATE 'latin1_swedish_ci',
-	`Nama Pegawai` VARCHAR(100) NOT NULL COLLATE 'latin1_swedish_ci',
-	`Username Pegawai` VARCHAR(60) NOT NULL COLLATE 'latin1_swedish_ci'
-) ENGINE=MyISAM;
-
--- Dumping structure for table db_puskesmas.user
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(60) NOT NULL,
@@ -148,19 +94,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table db_puskesmas.user: ~4 rows (approximately)
 DELETE FROM `user`;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`username`, `password`, `type`) VALUES
 	('adi', '12345', 'pegawai'),
 	('adip', '1234567', 'pasien'),
 	('adiparmawan2', '12345678', 'pasien'),
-	('wirnat', '1234567890', 'pasien');
+	('wirnat', '1234567890', 'pasien'),
+	('yanmastra', '1234567890', 'pasien'),
+	('yogadar', '123', 'pasien');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
--- Dumping structure for view db_puskesmas.antrian_aktif
 DROP VIEW IF EXISTS `antrian_aktif`;
--- Removing temporary table and create final VIEW structure
 DROP TABLE IF EXISTS `antrian_aktif`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `antrian_aktif` AS SELECT 
 dt_pasien.username AS 'Username',
@@ -168,26 +113,13 @@ id_antrian AS 'Nomor Antrian',
 nama_lengkap AS 'Nama Lengkap', 
 nik AS 'NIK', 
 keluhan AS 'Keluhan',
-tanggal_masuk AS 'Tanggal Masuk'
+tanggal_masuk AS 'Tanggal Masuk',
+`status` AS 'Status'
 FROM dt_antrian INNER JOIN dt_pasien ON dt_antrian.pasien = dt_pasien.nik
-WHERE dt_antrian.`status` = 'ngantre'
+WHERE dt_antrian.`status` = 'ngantre' OR dt_antrian.`status` = 'tidak datang'
 ORDER BY id_antrian ASC ;
 
--- Dumping structure for view db_puskesmas.antrian_tidak_datang
-DROP VIEW IF EXISTS `antrian_tidak_datang`;
--- Removing temporary table and create final VIEW structure
-DROP TABLE IF EXISTS `antrian_tidak_datang`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `antrian_tidak_datang` AS SELECT 
-dt_antrian.*,
-dt_pasien.nama_lengkap, 
-dt_pasien.alamat
-FROM dt_pasien INNER JOIN dt_antrian ON dt_antrian.pasien = dt_pasien.nik 
-WHERE dt_antrian.`status` = 'tidak datang' 
-ORDER BY dt_antrian.tanggal_masuk ASC ;
-
--- Dumping structure for view db_puskesmas.riwayat_antrian
 DROP VIEW IF EXISTS `riwayat_antrian`;
--- Removing temporary table and create final VIEW structure
 DROP TABLE IF EXISTS `riwayat_antrian`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `riwayat_antrian` AS SELECT 
 dt_pasien.username AS 'Username Pasien',
@@ -201,7 +133,7 @@ dt_pegawai.username AS 'Username Pegawai'
 FROM dt_pasien INNER JOIN dt_antrian ON dt_antrian.pasien = dt_pasien.nik 
 INNER JOIN dt_pegawai ON dt_antrian.pegawai = dt_pegawai.no_pegawai
 WHERE dt_antrian.`status` = 'keluar'
-ORDER BY dt_antrian.tanggal_keluar ;
+ORDER BY dt_antrian.tanggal_keluar DESC ;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
